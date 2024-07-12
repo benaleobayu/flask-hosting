6c472cc2964eafea650be9f1ecc0a59112e101b0
benaleobayu <benaleobs@gmail.com>
2024-07-12 14:00:36 +0700

    update main

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-from src.app import app as aplication
-=======
-import imp
-import os
-import sys
-
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-application = wsgi.application
->>>>>>> f485658d8716eb08bd427955c97863d2ba1b10ac
+from src.app import app as application
