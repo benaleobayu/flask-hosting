a95f58fddd8a5f4cb0366dd4a5d36fa999367390
benaleobayu <benaleobs@gmail.com>
2024-07-12 14:03:20 +0700

    update app users

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from src import create_app
 
+from src.modules.user.user_route import user_bp
+
 app = create_app()
+
+app.register_blueprint(user_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
