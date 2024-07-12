20e23725f9a99ed9af3bac1407635af5b3a3a4f4
benaleobayu <benaleobs@gmail.com>
2024-07-12 13:11:19 +0700

    update

--- src/app.py
+++ src/app.py
@@ -1,20 +1,6 @@
-import os
-import sys
-
 from src import create_app
-from src.modules.user.user_route import user_bp
 
 app = create_app()
 
-app.register_blueprint(user_bp)
-
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+if __name__ == '__main__':
+    app.run(debug=True)
